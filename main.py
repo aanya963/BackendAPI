753d04977c3268aefd2521b29fd4d9e2505dda3e
Aanya <aanyasah9@gmail.com>
2026-04-20 17:17:13 +0530

    Added request model + POST API in main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI # type: ignore
+from pydantic import BaseModel # type: ignore
 
 app = FastAPI()
+
+class analyzeRequest(BaseModel):
+    query: str
+    logs: list[str]
 
 @app.get("/")
 def read_root():
     return {"message": "Python service is running"}
+
+
+@app.get("/analyze")
+def analyze_data(req : analyzeRequest):
+    return{
+        "received_query": req.query,
+        "log_count": len(req.logs),
+        "summary": f"You sent {len(req.logs)} logs for analysis"
+    }
