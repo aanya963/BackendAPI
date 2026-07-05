713daab7525209204bbc2a5145ec1fb32bbbcd48
Aanya <aanyasah9@gmail.com>
2026-07-05 16:44:47 +0530

    test-700

--- Program.cs
+++ Program.cs
@@ -41,5 +41,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers(); // 👈 IMPORTANT (maps your AiController)
-Console.WriteLine("Branch 600");
+Console.WriteLine("Branch 700");
 app.Run();
