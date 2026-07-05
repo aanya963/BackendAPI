631328350953fa9d4bf9969baf45ef221c57c4c8
Aanya <aanyasah9@gmail.com>
2026-07-05 14:59:15 +0530

    test

--- Program.cs
+++ Program.cs
@@ -41,5 +41,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers(); // 👈 IMPORTANT (maps your AiController)
-Console.WriteLine("Branch 2");
+Console.WriteLine("Branch 20");
 app.Run();
